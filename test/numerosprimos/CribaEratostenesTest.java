869f6c73ab7fa5ee0f624f5c22d16701af609557
pablomanuel1999 <pablomanuelrg@gmail.com>
2023-03-03 13:22:38 +0100

    NumPrimos_2Commit_JUnit

---
+++ test/numerosprimos/CribaEratostenesTest.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package numerosprimos;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CribaEratostenesTest {
+    
+    @Test
+    public void testGenerarPrimos1() {
+        System.out.println("generarPrimos1");
+        int max = 0;
+        int[] expResult = {};
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertArrayEquals(expResult, result);
+        System.out.println("El resultado 1 coincide con el resultado esperado");
+    }
+    @Test
+    public void testGenerarPrimos2() {
+        System.out.println("generarPrimos2");
+        int max = 2;
+        int[] expResult = {2};
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertArrayEquals(expResult, result);
+        System.out.println("El resultado 2 coincide con el resultado esperado");
+    }
+    @Test
+    public void testGenerarPrimos3() {
+        System.out.println("generarPrimos3");
+        int max = 3;
+        int[] expResult = {2, 3};
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertArrayEquals(expResult, result);
+        System.out.println("El resultado 3 coincide con el resultado esperado");
+    }
+    @Test
+    public void testGenerarPrimos4() {
+        System.out.println("generarPrimos4");
+        int max = 100;
+        int[] expResult = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 
+                            47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        int[] result = CribaEratostenes.generarPrimos(max);
+        assertArrayEquals(expResult, result);
+        System.out.println("El resultado 4 coincide con el resultado esperado");
+    }
+    
+}
